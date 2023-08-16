357f5e287c7a287e4e5be1c07f90a82ab8b227ea
galaxyonthehub <117865433+galaxyonthehub@users.noreply.github.com>
2023-08-16 16:10:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function addUser() 
+{
+
+    userName = document.getElementById("player1NameInput").value;
+
+    localStorage.setItem("player1NameInput", player1NameInput);
+
+      window.location = "Test.html";
+
+}
